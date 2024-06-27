8382c69235555a872dfb6f5c5883992e73bc98be
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-06-27 13:43:05 -0700

    Add files via upload

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export const dynamic = 'force-dynamic'
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        {children}
+    </main>
+  );
+}
